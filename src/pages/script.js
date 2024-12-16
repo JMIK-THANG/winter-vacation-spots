3db05b7eea32cd8e54f21d2db26db0b9111b5bd4
Jmik Thang <jmikthang5@gmail.com>
2024-12-15 23:57:15 -0500

    js

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,57 +1,57 @@
 const initialCards = [
   {
     name: "Whiteface Mountain",
-    link: "https://image-tc.galaxy.tf/wijpeg-f3sp0ujcmj3zzmt97flvfmb86/whiteface-mountain-alpine-skiing_standard.jpg?crop=185%2C0%2C1631%2C1223",
+    link: "https://images.pexels.com/photos/4061996/pexels-photo-4061996.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
       "Whiteface has the greatest vertical east of the Rockies. More vertical equals longer runs, less time on lifts, no crowds, and endless fun. Whiteface is big, with a diversity of terrain for every level, offering a significant degree of steeps, with access to the slides off the Summit Quad.",
   },
   {
     name: "Gore Mountain",
-    link: "https://i0.wp.com/savingtimeinabottle.com/wp-content/uploads/2023/11/untitled-1-of-1-123-scaled.jpg?fit=2560%2C1707&ssl=1",
+    link: "https://images.pexels.com/photos/3714137/pexels-photo-3714137.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
       "Gore offers scenic skyrides and fun adventure activities during the summer and fall! Our cross-country trail network features snowmaking, grooming, and lights- snowshoers welcome on sides of trails too. Join us day or night at the historic North Creek Ski Bowl!",
   },
   {
     name: "Hunter Mountain",
-    link: "https://www.newyorkstatedestinations.com/catskills/hunter/images/hunter.jpg",
+    link: "https://images.pexels.com/photos/8627419/pexels-photo-8627419.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
       "with a summit that climbs to 3,200 ft. Hunter Mountain maintains renowned terrain across three separate mountains, with high-efficiency, state-of-the-art snowmaking that covers 100 percent of its trails, hence its 'snowmaking capital of the world' reputation.",
   },
   {
     name: "Belleayre Mountain",
-    link: "https://static01.nyt.com/images/2008/03/06/travel/escapes/belleayre_span.600.jpg?quality=75&auto=webp&disable=upscale",
+    link: "https://images.pexels.com/photos/6699199/pexels-photo-6699199.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
       "Belleayre is a family-friendly mountain, located in the heart of the Catskill Mountains, where you can find something for the entire family , long cruising trails, bumps, glades, parks, steeps, and a whole bunch of fun.",
   },
   {
     name: "Windham Mountain",
-    link: "https://static01.nyt.com/images/2023/12/08/nyregion/00ny-windham01/00ny-windham01-superJumbo.jpg",
+    link: "https://images.pexels.com/photos/848612/pexels-photo-848612.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
       "Located in New York's Northern Catskills, approximately 150 miles north of New York City and 50 miles south of Albany, Windham Mountain resort is known as a family-friendly resort offering an authentic ski experience and spectacular views in the Northeast region.",
   },
   {
     name: "Greek Peak",
-    link: "https://nyskiblog.com/wp-content/uploads/2016/06/greek-peak-ski-trail-map.jpg",
+    link: "https://images.pexels.com/photos/11485837/pexels-photo-11485837.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
       "Greek Peak Mountain Resort was founded in 1958 and has remained at the leading edge of the ski industry with forty-four ski trails and eleven glades, six chair lifts, two surface lifts, a tubing conveyor lift, a beginners' slope, and new and improved terrain parks.",
   },
   {
     name: "Peek'n Peak Resort",
-    link: "https://townsquare.media/site/10/files/2024/10/attachment-peak-n-peak.jpg?w=780&q=75",
+    link: "https://images.pexels.com/photos/848682/pexels-photo-848682.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
       "Peek'n Peak Resort is a versatile destination located in southwestern New York, offering activities year-round",
   },
   {
-    name: "HoliMont",
-    link: "https://images.unsplash.com/photo-1610479201125-a5c7f17370a8?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Holiday Valley",
+    link: "https://images.pexels.com/photos/869258/pexels-photo-869258.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
-      "Located in Western New York, Holimont is the largest private ski resort in the US. With 700' of wall-to-wall snowmaking and state-of-the-art grooming, you're guaranteed to always have an excellent day on the slopes",
+      "Located in Western New York, Holiday Valley ski resort is a year-round outdoor playground, providing varied terrain for skiers and boarders as well as golfing, an adventure park, ziplines, spa services and more. The resort is designed to be appropriate for visitors of all ages and is a go-to for families.",
   },
   {
-    name: "Holiday Mountain",
-    link: "https://images.unsplash.com/photo-1705205148375-a845e74dbe4b?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Beartown Ski Area",
+    link: "https://images.pexels.com/photos/3193846/pexels-photo-3193846.jpeg?auto=compress&cs=tinysrgb&w=600",
     description:
-      "Holiday Mountain Ski and Fun Park is a family-friendly destination in the Southern Catskills of New York that offers skiing, snowboarding, tubing, and a fun park",
+      "The ski resort Beartown is located in The Adirondacks (USA, New York). For skiing and snowboarding, there are 0.8 km of slopes available. 2 lifts transport the guests. The winter sports area is situated between the elevations of 263 and 304 m.",
   },
 ];
 
